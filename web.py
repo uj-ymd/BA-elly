f2bf5d4bcf975cbcfe9c548bc41d4ac5e678444e
uj-ymd <myelly2005@gmail.com>
2026-04-30 12:16:56 +0800

    Add files via upload

--- web.py
+++ web.py
@@ -36,7 +36,48 @@ def index():
     link += "<a href=/read2>靜宜資管老師查詢</a><hr>"
     link += "<a href=/spider1>爬取子青老師本學期課程</a><hr>"
     link += "<a href=/movie1>爬取即將上映電影</a><hr>"
+    link += "<a href=/spidermovie>爬取即將上映電影</a><hr>"
     return link
+
+@app.route("/spidermovie")
+def spidermovie():
+    R = ""
+    db = firestore.client()
+
+    import requests
+    from bs4 import BeautifulSoup
+    url = "http://www.atmovies.com.tw/movie/next/"
+    Data = requests.get(url)
+    Data.encoding = "utf-8"
+
+    sp = BeautifulSoup(Data.text, "html.parser")
+    lastUpdate = sp.find(class_="smaller09").text.replace("更新時間：", "")
+
+    result=sp.select(".filmListAllX li")
+    total = 0
+    for item in result:
+        total += 1 
+        movie_id = item.find("a").get("href").replace("/movie/", "").replace("/", "")
+        title = item.find(class_="filmtitle").text
+        picture = "https://www.atmovies.com.tw"+ item.find("img").get("src")
+        hyperlink = "https://www.atmovies.com.tw"+ item.find("a").get("href")
+        showDate = item.find(class_="runtime").text[5:15]
+
+        doc = {
+            "title": title,
+            "picture": picture,
+            "hyperlink": hyperlink,
+            "showDate": showDate,
+            "lastUpdate": lastUpdate
+        }
+
+        doc_ref = db.collection("電影2B").document(movie_id)
+        doc_ref.set(doc)
+
+        R += "網站最近更新日期：" + lastUpdate + "<br>"
+        R += ("總共爬取" + str(total) + "部電影到資料庫")
+    return R
+
 
 @app.route("/movie1", methods=['GET', 'POST'])
 def movie1():
